338c549c805576efd8e305ad1072bd3fcd481a97
lost-in-noise <nancykikvadze@gmail.com>
2026-01-03 19:06:17 +0400

    void method with if

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,23 +3,24 @@ package org.example;
 public class Main {
     static void main() {
 
-//        String fullName = "My name is Nancy Kikvadze";
-//        System.out.println(fullName.contains("Nancy"));
-//        System.out.println(fullName.length());
-//        printName();
-//        printLastName();
+        String fullName = "My name is Nancy Kikvadze";
+        System.out.println(fullName.contains("Nancy"));
+        System.out.println(fullName.length());
+        printName();
+        printLastName();
         printNameAndAge("Nancy", 38);
         int c = sumOfNumbers(5, 3);
         System.out.println(c);
+        takeLoan(25);
     }
 
-//    static void printName() {
-//        System.out.println("My name is Nancy");
-//    }
-//
-//    static void printLastName() {
-//        System.out.println("My last name is Kikvadze");
-//    }
+    static void printName() {
+        System.out.println("My name is Nancy");
+    }
+
+    static void printLastName() {
+        System.out.println("My last name is Kikvadze");
+    }
 
     static void printNameAndAge(String userName, int userAge) {
         System.out.println("my name is " + userName + " and my age is " + userAge);
@@ -27,5 +28,15 @@ public class Main {
 
     static int sumOfNumbers(int a, int b) {
         return a + b;
+    }
+
+    static void takeLoan(int age) {
+        if (age < 20) {
+            System.out.println("ვერ აიღებ სესხს, რადგან 20-ზე ნაკლები ხარ ასაკით");
+        } else if (age >= 20 & age <= 65) {
+            System.out.println("შენ შეგიძლია სესხის აღება");
+        } else {
+            System.out.println("ვერ აიღებ სესხს, რადგან ხარ 65+");
+        }
     }
 }
