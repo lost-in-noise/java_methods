03f2435f1d90b1e34924310bfd005d199ec2bb86
lost-in-noise <nancykikvadze@gmail.com>
2026-01-03 18:00:10 +0400

    void methods parameters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,18 +3,23 @@ package org.example;
 public class Main {
     static void main() {
 
-        String fullName = "My name is Nancy Kikvadze";
-        System.out.println(fullName.contains("Nancy"));
-        System.out.println(fullName.length());
-        printName();
-        printLastName();
+//        String fullName = "My name is Nancy Kikvadze";
+//        System.out.println(fullName.contains("Nancy"));
+//        System.out.println(fullName.length());
+//        printName();
+//        printLastName();
+        printNameAndAge("Nancy", 38);
     }
 
-    static void printName() {
-        System.out.println("My name is Nancy");
-    }
+//    static void printName() {
+//        System.out.println("My name is Nancy");
+//    }
+//
+//    static void printLastName() {
+//        System.out.println("My last name is Kikvadze");
+//    }
 
-    static void printLastName() {
-        System.out.println("My last name is Kikvadze");
+    static void printNameAndAge(String userName, int userAge) {
+        System.out.println("my name is " + userName + "My age is " + userAge);
     }
 }
