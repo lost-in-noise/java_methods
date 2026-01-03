c1b550087a8586f1b9972e25b99f7337f30e75e6
lost-in-noise <nancykikvadze@gmail.com>
2026-01-03 17:35:15 +0400

    void methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,18 @@ package org.example;
 public class Main {
     static void main() {
 
-      String fullName = "My name is Nancy Kikvadze";
+        String fullName = "My name is Nancy Kikvadze";
+        System.out.println(fullName.contains("Nancy"));
+        System.out.println(fullName.length());
+        printName();
+        printLastName();
+    }
 
+    static void printName() {
+        System.out.println("My name is Nancy");
+    }
+
+    static void printLastName() {
+        System.out.println("My last name is Kikvadze");
     }
 }
